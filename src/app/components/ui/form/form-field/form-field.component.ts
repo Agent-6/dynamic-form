742f0ca5c71c384a5bf9006bfc4343de19e49a35
mfaour <92956173+Agent-6@users.noreply.github.com>
2026-01-14 23:00:07 +0000

    composition for form fields

--- src/app/components/ui/form/form-field/form-field.component.ts
+++ src/app/components/ui/form/form-field/form-field.component.ts
@@ -1,38 +1,77 @@
-import { ChangeDetectionStrategy, Component, contentChild, InjectionToken, input, ViewEncapsulation } from '@angular/core';
-import { FormValueControl } from '@angular/forms/signals';
+import { ChangeDetectionStrategy, Component, computed, contentChild, Directive, InjectionToken, input, InputSignal, InputSignalWithTransform, model, ModelSignal, OutputRef, Provider, ViewEncapsulation } from '@angular/core';
+import { DisabledReason, FormValueControl, ValidationError, WithOptionalField } from '@angular/forms/signals';
 import { NgpFormField } from 'ng-primitives/form-field';
-
-export const FORM_CONTROL_TOKEN = new InjectionToken<FormValueControl<any>>('FormValueControl');
 
 @Component({
   selector: 'ui-form-field',
   template: `
-    <div ngpFormField class="w-full flex flex-col gap-1">
-      <!-- Label -->
-      <label ngpLabel class="text-sm font-medium text-gray-700 dark:text-gray-200">
-        {{ label() }}
-        @if (field().required?.()) {
-          <span class="ml-1 font-medium">&nbsp;*</span>
+    @if (!hidden()) {
+      <div ngpFormField class="w-full flex flex-col gap-1">
+        <!-- Label -->
+        <label ngpLabel [for]="name()" class="text-sm font-medium text-gray-700 dark:text-gray-200">
+          {{ label() }}
+          @if (required()) {
+            <span class="font-medium">*</span>
+          }
+        </label>
+  
+        <!-- Description -->
+        @if (description()) {
+          <p ngpDescription class="mb-1 text-xs text-gray-600 dark:text-gray-100" >
+            {{ description() }}
+          </p>
         }
-      </label>
-    
-      <!-- Description -->
-      @if (description()) {
-        <p ngpDescription class="mb-1 text-xs text-gray-600 dark:text-gray-100" >
-          {{ description() }}
-        </p>
-      }
+  
+        <!-- Field Control -->
+        <ng-content />
 
-      <!-- the field control, has to implement 'FormValueControl' -->
-      <ng-content />
-    </div>
+        <!-- Errors -->
+        @if (invalid()) {
+          @for (error of errors(); track $index) {
+            <span class="text-sm text-red-500">{{ error.message }}</span>
+          }
+        }
+      </div>
+    }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   imports: [NgpFormField]
 })
 export class FormFieldComponent {
-  field = contentChild.required(FORM_CONTROL_TOKEN);
-  label = input.required<string>();
-  description = input<string>();
+  protected readonly control = contentChild.required(FormFieldControlComponent);
+  
+  protected readonly label = computed(() => this.control().label());
+  protected readonly description = computed(() => this.control().description());
+  
+  protected readonly name = computed(() => this.control().name());
+  protected readonly readonly = computed(() => this.control().readonly());
+  protected readonly required = computed(() => this.control().required());
+  protected readonly hidden = computed(() => this.control().hidden());
+
+  protected readonly invalid = computed(() => this.control().invalid());
+  protected readonly errors = computed(() => this.control().errors());
+}
+
+@Component({
+  template: '',
+})
+export abstract class FormFieldControlComponent<T = any> implements FormValueControl<T> {
+  readonly label = input.required<string>();
+  readonly description = input<string>();
+  readonly placeholder = input<string>();
+
+  readonly value = model.required<T>();
+  readonly touched = model<boolean>(false);
+
+  readonly name = input.required<string>();
+  readonly readonly = input<boolean>(false);
+  readonly required = input<boolean>(false);
+  readonly hidden = input<boolean>(false);
+
+  readonly disabled = input<boolean>(false);
+  readonly disabledReasons = input<readonly WithOptionalField<DisabledReason>[]>([]);
+
+  readonly invalid = input<boolean>(false);
+  readonly errors = input<readonly  WithOptionalField<ValidationError>[]>([]);
 }
--- src/app/components/ui/select/select.component.ts
+++ src/app/components/ui/select/select.component.ts
@@ -1,14 +1,11 @@
-import { ChangeDetectionStrategy, Component, computed, input, InputSignal, InputSignalWithTransform, model, signal, ViewEncapsulation } from '@angular/core';
-import { FormValueControl } from '@angular/forms/signals';
+import { ChangeDetectionStrategy, Component, computed, input, model, signal, ViewEncapsulation } from '@angular/core';
 import {
   NgpSelect,
   NgpSelectDropdown,
   NgpSelectOption,
   NgpSelectPortal,
 } from 'ng-primitives/select';
-import { FormFieldComponent } from "../form/form-field/form-field.component";
-import { FormFieldControlComponent } from "../form/form-field/form-field-control.component";
-import { FormFieldLabelComponent } from '../form/form-field/form-filed-label.component';
+import { FormFieldComponent, FormFieldControlComponent } from "../form/form-field/form-field.component";
 
 @Component({
   selector: 'ui-select',
@@ -16,28 +13,16 @@ import { FormFieldLabelComponent } from '../form/form-field/form-filed-label.com
   styleUrl: './select.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
-  imports: [NgpSelect, NgpSelectDropdown, NgpSelectOption, NgpSelectPortal, FormFieldComponent, FormFieldLabelComponent, FormFieldControlComponent],
+  imports: [NgpSelect, NgpSelectDropdown, NgpSelectOption, NgpSelectPortal, FormFieldComponent],
 })
-export class SelectComponent implements FormValueControl<string | undefined> {
+export class SelectComponent extends FormFieldControlComponent<string | undefined> {
   /** The options for the select. */
   readonly options = input.required<{ id: string; name: string; }[]>();
-  readonly label = input.required<string>();
-
-  /** The selected value. */
-  readonly value = model<string>();
-
-  readonly required = input(true);
-
+  
   protected readonly open = signal<boolean>(false);
   protected readonly valueLabel = computed(() => {
     const id = this.value();
     const option = this.options().find(o => o.id === id);
     return option?.name;
-  })
-
-  /** The placeholder for the input. */
-  readonly placeholder = input<string>('');
-
-  /** The disabled state of the select. */
-  readonly disabled = input<boolean>(false);
+  });
 }
